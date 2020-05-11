e4696eb1e670e03e26dbed024c069eff22b40331
gamalielrt <gamalielrt@gmail.com>
2020-05-11 12:23:43 +0100

    z

--- index.php
+++ index.php
@@ -1,3 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv=“Pragma” content=”no-cache”>
+    <meta http-equiv=“Expires” content=”-1″>
+    <meta http-equiv=“CACHE-CONTROL” content=”NO-CACHE”>
+
+<title>Page Title</title>
+<link rel="stylesheet" href="main.css">
+
+</head>
+<body>
+<?php
+echo 'Hello PHP';
+?>
+    <div id="chat-wrap"><div id="chat-area"></div></div>
+<div id="chatinput">
+    <label id="typebox">
+        <form><input id="typeboxinput" ></forma> 
+     </label>
+     <!--<label id="commentbutton">  -->     
+        <button  id="commentbuttonhit" >Comment</button>
+     <!--</label>-->
+
+</div>
+</body>
+</html>
 <?php
 echo 'Hello PHP';
 ?>
